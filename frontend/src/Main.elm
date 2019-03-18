af1f116e32857f20acdb82effd21300525f38a2e
Brad Cypert <brad.cypert@gmail.com>
2019-03-18 18:06:39 -0400

    fix nav bar

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -79,73 +79,11 @@ view : Model -> Browser.Document Msg
 view model =
   { title = "Charity Code - Helping Nonprofits find the development help they need!"
   , body =
-      [ navBar
-      ,text "The current URL is: "
+      [ NavBar.view {}
+      , text "The current URL is: "
       , b [] [ text (Url.toString model.url) ]
-      , ul []
-          [ viewLink "/home"
-          , viewLink "/profile"
-          , viewLink "/reviews/the-century-of-the-self"
-          , viewLink "/reviews/public-opinion"
-          , viewLink "/reviews/shah-of-shahs"
-          ]
       ]
   }
-
-
-navBar = 
-    nav [ attribute "aria-label" "main navigation", class "navbar", attribute "role" "navigation" ]
-    [ div [ class "navbar-brand" ]
-        [ a [ class "navbar-item", href "#" ]
-            [ img [ attribute "height" "28", src "https://bulma.io/images/bulma-logo.png", attribute "width" "112" ]
-                []
-            ]
-        , a [ attribute "aria-expanded" "false", attribute "aria-label" "menu", class "navbar-burger burger", attribute "data-target" "navbarBasicExample", attribute "role" "button" ]
-            [ span [ attribute "aria-hidden" "true" ]
-                []
-            , span [ attribute "aria-hidden" "true" ]
-                []
-            , span [ attribute "aria-hidden" "true" ]
-                []
-            ]
-        ]
-    , div [ class "navbar-menu", id "navbarBasicExample" ]
-        [ div [ class "navbar-start" ]
-            [ a [ class "navbar-item" ]
-                [ text "Home      " ]
-            , a [ class "navbar-item" ]
-                [ text "Why      " ]
-            , div [ class "navbar-item has-dropdown is-hoverable" ]
-                [ a [ class "navbar-link" ]
-                    [ text "More        " ]
-                , div [ class "navbar-dropdown" ]
-                    [ a [ class "navbar-item" ]
-                        [ text "About          " ]
-                    , a [ class "navbar-item" ]
-                        [ text "Jobs          " ]
-                    , a [ class "navbar-item" ]
-                        [ text "Contact          " ]
-                    , hr [ class "navbar-divider" ]
-                        []
-                    , a [ class "navbar-item" ]
-                        [ text "Report an issue          " ]
-                    ]
-                ]
-            ]
-        , div [ class "navbar-end" ]
-            [ div [ class "navbar-item" ]
-                [ div [ class "buttons" ]
-                    [ a [ class "button is-primary" ]
-                        [ strong []
-                            [ text "Sign up" ]
-                        ]
-                    , a [ class "button is-light" ]
-                        [ text "Log in          " ]
-                    ]
-                ]
-            ]
-        ]
-    ]
 
 viewLink : String -> Html msg
 viewLink path =
