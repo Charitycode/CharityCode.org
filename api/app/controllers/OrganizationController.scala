e802f3199c3566f4730190c2508c645ed4a876f9
Jeremy Williams <jeremy.tyler.williams@gmail.com>
2019-05-30 19:50:38 -0400

    whoops

--- api/app/controllers/OrganizationController.scala
+++ api/app/controllers/OrganizationController.scala
@@ -19,6 +19,11 @@ class OrganizationController @Inject()(cc: ControllerComponents) extends Abstrac
     * a path of `/`.
     */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index()) }
+  def index(): Action[AnyContent] = Action.async {
+    implicit request: Request[AnyContent] =>
+      organizationRepo.list().map { organizations =>
+        Ok(Json.toJson(organizations))
+      }
   }
 }
