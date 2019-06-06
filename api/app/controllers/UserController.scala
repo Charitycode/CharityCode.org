941dfcd353cf34abfbec22c3ff6b9f541a1beed8
Jeremy Williams <jeremy.tyler.williams@gmail.com>
2019-06-06 18:35:56 -0400

    controller and repo for user

--- api/app/controllers/UserController.scala
+++ api/app/controllers/UserController.scala
@@ -1,15 +1,20 @@
 package controllers
 
+import dal._
 import javax.inject._
 import play.api._
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class UserController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class UserController @Inject()(cc: ControllerComponents,
+                               userRepo: UserRepo)
+  extends AbstractController(cc) {
 
   /**
     * Create an Action to render an HTML page.
@@ -18,7 +23,10 @@ class UserController @Inject()(cc: ControllerComponents) extends AbstractControl
     * will be called when the application receives a `GET` request with
     * a path of `/`.
     */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index(): Action[AnyContent] = Action.async {
+    implicit request: Request[AnyContent] =>
+      userRepo.list().map { users =>
+        Ok(Json.toJson(users))
+      }
   }
 }
